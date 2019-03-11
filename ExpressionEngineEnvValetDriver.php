c9b7c722002e9eaec34a6fcc2bee26a89f041070
Glen Gringgo Bangkila <hadeinvane@gmail.com>
2019-03-11 10:03:57 +0800

    Add ExpressionEngineEnvValetDriver class

---
+++ ExpressionEngineEnvValetDriver.php
@@ -0,0 +1,60 @@
+<?php
+
+class ExpressionEngineEnvValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        if (is_dir($sitePath . '/system') && is_dir($sitePath . '/system/ee')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/public/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        
+        if ($uri == '/system' || $uri == '/system/') {
+            header('Location: /admin.php'); die;
+        }
+
+        if ($uri == '/admin.php') {
+            return $sitePath.'/public/admin.php';
+        }
+
+        return $sitePath.'/public/index.php';
+    }
+}
